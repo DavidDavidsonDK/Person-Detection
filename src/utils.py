048df018d6d6625edb83b043f97d879724ad39e0
Davit Karamyan <d.kprogrammer0.0@gmail.com>
2018-12-18 15:23:30 +0400

    push scaleing

---
+++ src/utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+def bbox_area(bbox):
+    return (bbox[:,2] - bbox[:,0]) * (bbox[:,3] - bbox[:,1])
+def clip_box(bbox, clip_box, alpha):
+    """Clip the bounding boxes to the borders of an image
+    Parameters
+    ----------
+    bbox: numpy.ndarray
+        Numpy array containing bounding boxes of shape `N X 4` where N is the 
+        number of bounding boxes and the bounding boxes are represented in the
+        format `x1 y1 x2 y2`
+    
+    clip_box: numpy.ndarray
+        An array of shape (4,) specifying the diagonal co-ordinates of the image
+        The coordinates are represented in the format `x1 y1 x2 y2`
+        
+    alpha: float
+        If the fraction of a bounding box left in the image after being clipped is 
+        less than `alpha` the bounding box is dropped. 
+    
+    Returns
+    -------
+    numpy.ndarray
+        Numpy array containing **clipped** bounding boxes of shape `N X 4` where N is the 
+        number of bounding boxes left are being clipped and the bounding boxes are represented in the
+        format `x1 y1 x2 y2` 
+    
+    """
+    ar_ = (bbox_area(bbox))
+    x_min = np.maximum(bbox[:,0], clip_box[0]).reshape(-1,1)
+    y_min = np.maximum(bbox[:,1], clip_box[1]).reshape(-1,1)
+    x_max = np.minimum(bbox[:,2], clip_box[2]).reshape(-1,1)
+    y_max = np.minimum(bbox[:,3], clip_box[3]).reshape(-1,1)
+    
+    bbox = np.hstack((x_min, y_min, x_max, y_max, bbox[:,4:]))
+    
+    delta_area = ((ar_ - bbox_area(bbox))/ar_)
+    
+    mask = (delta_area < (1 - alpha)).astype(int)
+    
+    bbox = bbox[mask == 1,:]
+
+
+    return bbox
